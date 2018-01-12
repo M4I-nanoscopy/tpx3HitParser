06cd4d06f4bb76a21239aa50dfd93f330b25840b
Paul van Schayck <polleke@gmail.com>
2018-01-12 08:33:11 +0100

    Show different chips in SPIDR timer plot

--- frames/output.py
+++ frames/output.py
@@ -1,13 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from lib.constants import *
-from matplotlib.widgets import Slider
+from matplotlib.ticker import EngFormatter
 
 
-# Display some stats about the SPIDR global timer
+# Display some stats about the SPIDR global time
 def spidr_time_stats(events):
-    # events = events[()]
-
     spidr = events['TSPIDR']
 
     tick = 26.843 / 65536.
@@ -17,20 +14,40 @@ def spidr_time_stats(events):
     print "SPIDR time min: %d" % spidr.min()
     print "SPIDR time max: %d" % spidr.max()
 
-    print "Indeces where SPIDR time jumps: "
-    #diff = np.where(abs(np.diff(spidr)) > 1000000)
-    #print diff
+    # print "Indices where SPIDR time jumps: "
+    # diff = np.where(abs(np.diff(spidr)) > 1000000)
+    # print diff
 
-    print "Seconds (guess):"
-    # print tick
-    #print (spidr[-1] - spidr[0]) * tick + len(diff) * 26.843
-    print (spidr[-1] - spidr[0]) * tick
+    print "Seconds exposure time (guess):"
+    # print (spidr[-1] - spidr[0]) * tick + len(diff) * 26.843
+    print float(spidr[-1] - spidr[0]) * tick
 
     plot_timers(events)
 
 
 # Plot SPIDR time of entire run
 def plot_timers(events):
-    spidr = events['TSPIDR']
-    plt.scatter(np.arange(len(spidr)), spidr)
+    fig, ax = plt.subplots()
+
+    index = np.arange(len(events))
+
+    for chip in range(0, 4):
+        # Index frame to only the particular chip
+        chip_events = events[[events['chipId'] == chip]]
+        chip_index = index[[events['chipId'] == chip]]
+
+        # Get only every 1000nth hit
+        spidr = chip_events['TSPIDR'][1::1000]
+        spidr_index = chip_index[1::1000]
+
+        plt.scatter(spidr_index, spidr, label='Chip %d' % chip)
+
+    plt.title('SPIDR time (every 1000nth hit)')
+
+    formatter0 = EngFormatter(unit='hit')
+    ax.xaxis.set_major_formatter(formatter0)
+
+    plt.xlabel('Hit index')
+    plt.ylabel('SPIDR time ticks')
+    plt.legend()
     plt.show()
