de8adcdebbb159c7660ee70048a430ca2baf1bdb
Paul van Schayck <polleke@gmail.com>
2019-03-15 14:52:13 +0100

    Correct random method

--- events/chip_edge_correct.py
+++ events/chip_edge_correct.py
@@ -23,19 +23,19 @@ def chip_edge_correct(events):
 
     # Take second horizontal row and redivide over three pixels
     x_edge1 = np.logical_and(ex > 256, ex < 257)
-    ex[x_edge1] = ex[x_edge1] + np.random.uniform(2, 4, len(ex[x_edge1]))
+    ex[x_edge1] = ex[x_edge1] + np.random.random_integers(2, 4, len(ex[x_edge1]))
 
     # Take first horizontal row and redivide over three pixels
     x_edge2 = np.logical_and(ex > 255, ex < 256)
-    ex[x_edge2] = ex[x_edge2] + np.random.uniform(0, 2, len(ex[x_edge2]))
+    ex[x_edge2] = ex[x_edge2] + np.random.random_integers(0, 2, len(ex[x_edge2]))
 
     # Take second vertical column and redivide over three pixels
     y_edge1 = np.logical_and(ey > 256, ey < 257)
-    ey[y_edge1] = ey[y_edge1] + np.random.uniform(2, 4, len(ey[y_edge1]))
+    ey[y_edge1] = ey[y_edge1] + np.random.random_integers(2, 4, len(ey[y_edge1]))
 
     # Take second vertical column and redivide over three pixels
     y_edge2 = np.logical_and(ey > 255, ey < 256)
-    ey[y_edge2] = ey[y_edge2] + np.random.uniform(0, 2, len(ey[y_edge2]))
+    ey[y_edge2] = ey[y_edge2] + np.random.random_integers(0, 2, len(ey[y_edge2]))
 
     # Place back new coordinates in event
     e['x'] = ex
